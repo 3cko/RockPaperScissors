10b9240a112d62b6f8dc0b37ea7f21412ce27e27
ecko <ecko@mbp.(none)>
2013-03-14 21:40:25 -0700

    added user input + validation

--- RPC.java
+++ RPC.java
@@ -9,27 +9,40 @@ public class RPC
 {
     public static void main(String[] args)
     {
-        String [] choices = getChoices();
-        System.out.print(choices);
+    	String [] choices = getChoices();
+		String choice = "false";
+		while ("false".equals(choice))
+		{
+			choice = getUserInput(choices);
+		}
+		System.out.println(choice);
+
     }
 
 //    public static String getNpcChoice()
 //    {
-//        return;
 //    }
 //
 //    public static String doBattle(String PC, String NPC)
 //    {
-//        return;
 //    }
-//
-//    public static String getUserInput()
-//    {
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("[R]ock, [P]aper, [S]cissors (Choose One): ");
-//        String choice = input.nextLine();
-//        return;
-//    }
+
+    public static String getUserInput(String[] valid)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("[R]ock, [P]aper, [S]cissors (Choose One): ");
+        String choice = input.next();
+		choice = choice.toLowerCase();
+		for (int x = 0; x < valid.length; x++)
+		{
+			if (choice.equals(valid[x]))
+			{
+				return choice;
+			}
+		}
+		System.out.println("Invalid choice! Try again.");
+		return "false";
+    }
 
     public static String[] getChoices()
     {
